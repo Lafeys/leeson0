6e8300685ed1305b020006ad76a499dd14cbda9b
Lafeys <latypovslava99@gmail.com>
2025-01-30 21:15:23 +0500

    Add files via upload

---
+++ note_manager файл add_list.py
@@ -0,0 +1,18 @@
+tatls = []
+
+tatl1 = input("Введите первый заголовок: ")
+tatls.append(tatl1)
+
+tatl2 = input("Введите второй заголовок: ")
+tatls.append(tatl2)
+
+tatle3 = input("Введите третий заголовок: ")
+tatls.append(tatle3)
+
+note_name = input("Введите название заметки: ")
+
+note_text = input("Введите текст заметки: ")
+
+print("Заголовки:", tatls)
+print("Название заметки:", note_name)
+print("Текст заметки:", note_text)
